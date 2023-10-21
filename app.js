100cb39cf93324d74993b0c6d0ebb9e56f3f940a
PMehar <pujamehar05@gmail.com>
2023-10-21 11:07:27 +0530

    Cleaned up the code , added 404 error page and made routes using separate filters

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express =  require("express");
 const bodyParser =  require('body-parser');
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+
 const app =  express ();
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.use("/add-product",(req, res,next)=>{
-    res.send('<form action="/product" method= "POST"><input type="text"name= "title" id = "title" placeholder = "Enter Product"><input type="number"name= "size" id = "size" placeholder = "Enter Size"><br><br><button type = "submit">Add Product</button> </input> </form>');
-});
+app.use("/admin", adminRoutes);
+app.use("/shop", shopRoutes);
 
-app.post("/product",(req, res,next)=>{
-    console.log(req.body);
-    res.redirect("/");
-});
-
-app.use("/",(req, res,next)=>{
-    res.send("<h1>Hello From Express !</h1>");
-});
+app.use((req, res, next)=>{
+    res.status(404).send("<h1>Page Not Found</h1>")
+})
 
 app.listen(3000);
